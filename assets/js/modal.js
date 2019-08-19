3566e4b2bbcde51db9a4b3090580ac90223f8f84
Gabriela <gabismauricio@gmail.com>
2019-08-19 01:46:28 -0300

    :bug: fix open modal on loading images

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,22 +1,20 @@
-window.onload = () => {
-  const modal = document.querySelector('.gsm-modal');
-  // const content = document.getElementById('modalContent');
-  // const left = document.querySelector('.gsm-modal__content__arrow._left');
-  // const right = document.querySelector('.gsm-modal__content__arrow._right');
-  const img = document.getElementById('modalImg');
-  const modalTrigger = document.querySelectorAll("[gsm-modal-trigger]");
-  const modalClose = document.querySelector(".gsm-modal__close");
+const modal = document.querySelector('.gsm-modal');
+// const content = document.getElementById('modalContent');
+// const left = document.querySelector('.gsm-modal__content__arrow._left');
+// const right = document.querySelector('.gsm-modal__content__arrow._right');
+const img = document.getElementById('modalImg');
+const modalTrigger = document.querySelectorAll("[gsm-modal-trigger]");
+const modalClose = document.querySelector(".gsm-modal__close");
 
-  modalTrigger.forEach(function (el, id) {
-    el.addEventListener('click', function (event) {
-      event.preventDefault();
-      img.src = el.src
-      modal.classList.add('_open');
-    });
-  })
-
-  modalClose.addEventListener('click', function (event) {
+modalTrigger.forEach(function (el, id) {
+  el.addEventListener('click', function (event) {
     event.preventDefault();
-    modal.classList.remove('_open');
+    img.src = el.src
+    modal.classList.add('_open');
   });
-}
+})
+
+modalClose.addEventListener('click', function (event) {
+  event.preventDefault();
+  modal.classList.remove('_open');
+});
